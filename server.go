a4afbbeb0eaa4c12eef578a3fef335087208b2c2
[yeawyow] <[yeawyow@gmail.com]>
2021-01-25 20:10:31 +0700

    first commit

---
+++ server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	//"main/api"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	//กำหนด static folder
+	router.Static("/images", "./uploaded/images")
+	//api.Setup(router)
+	router.Run(":8081")
+}
